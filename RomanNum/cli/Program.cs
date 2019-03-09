f464e36cfe8220f86331023347d93feda6870913
Varmetek <xdman500@gmail.com>
2019-03-09 18:16:28 -0500

    Adding source files

---
+++ RomanNum/cli/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RomanNumProject;
+
+namespace RomanNumProjectCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter \"quit\" to exit");
+            const string EXIT = "quit";
+
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                string[] vals = input.Split(' ');
+
+                if (vals[0].Equals(EXIT))
+                {
+                    Console.WriteLine("Exiting program");
+                    break;
+                }
+                else if (vals[0].Equals("toroman"))
+                {
+                    if (vals.Length == 2)
+                    {
+                        string read = vals[1];
+                        int value;
+
+                        if (!int.TryParse(read, out value))
+                        {
+                            Console.WriteLine("Could not read the number.");
+                            Console.ReadKey();
+                            continue;
+                        }
+
+
+                        string romans = RomanConverter.toRoman(value);
+
+                        int convValue = RomanConverter.ToDecimal(romans);
+                        Console.WriteLine("Roman Numeral: " + romans);
+                        Console.WriteLine("Decimal Value: " + convValue);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not enough arguments: toroman <number>");
+                    }
+                }
+                else if (vals[0].Equals("todecimal"))
+                {
+
+                    if (vals.Length == 2)
+                    {
+
+                        string roman = vals[1];
+
+                        try
+                        {
+                            int convValue = RomanConverter.ToDecimal(roman);
+                            string romans = RomanConverter.toRoman(convValue);
+                            Console.WriteLine("Roman Numeral: " + romans);
+                            Console.WriteLine("Decimal Value: " + convValue);
+                        }
+                        catch (System.ArgumentException ex)
+                        {
+
+                            Console.WriteLine("Unrecognized character in the sequence");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not enough arguments: todecimal <roman>");
+
+
+
+                        Console.ReadKey();
+                    }
+                }
+                else if (vals[0].Equals("countdown"))
+                {
+
+                    if (vals.Length == 2)
+                    {
+
+                        string num = vals[1];
+                        int value;
+
+                        if (!int.TryParse(num, out value))
+                        {
+                            Console.WriteLine("Could not read the number.");
+                            Console.ReadKey();
+                            continue;
+                        }
+
+                        countDown(value);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not enough arguments: countdown <number>");
+
+
+
+                        Console.ReadKey();
+                    }
+                }
+                else if (vals[0].Equals("help"))
+                {
+                    Console.WriteLine("RomanNum help menu");
+                    Console.WriteLine("toroman <number> - Converts a decimal number to a roman numeral");
+                    Console.WriteLine("todecimal <roman> - Converts a roman numeral to a decimal number");
+                    Console.WriteLine("countdown <number> - Displays all roman numerals from the given number to 1");
+                    Console.WriteLine("help - Displays this help menu");
+                }
+                else
+                {
+                    Console.WriteLine("Unknown command");
+                }
+            }
+        }
+               
+
+
+        static void standard() {
+
+            Console.WriteLine("Give me a number");
+            string input = Console.ReadLine();
+            int value;
+
+            if (!int.TryParse(input, out value))
+            {
+                Console.WriteLine("Could not read the number.");
+                Console.ReadKey();
+                return;
+            }
+
+
+            string romans = RomanConverter.toRoman(value);
+
+            int convValue = RomanConverter.ToDecimal(romans);
+            Console.WriteLine("Roman Numeral: " + romans);
+            Console.WriteLine("Decimal Value: " + convValue);
+        }
+
+
+        static void countDown(int count) {
+            for (int i = count; i > 0; i--) {
+                string romans = RomanConverter.toRoman(i);
+                int value = RomanConverter.ToDecimal(romans);
+
+
+                Console.WriteLine($"{i,4} {romans,15} {value,4}");
+
+            }
+        }
+    }
+}
---
+++ RomanNum/program/RomanConverter.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumProject
+{
+
+    /// <summary>
+    /// A collection of functions that help convert inbetween the decimal and roman number systems.
+    /// </summary>
+    public class RomanConverter
+    {
+
+        private static Numeral[] numerals;
+
+        static RomanConverter() {
+
+            numerals = new Numeral[] {
+                   new Numeral('M',1000),
+                   new Numeral('D',500),
+                   new Numeral('C',100),
+                   new Numeral('L',50),
+                   new Numeral('X',10),
+                   new Numeral('V',5),
+                   new Numeral('I',1)
+
+               };
+        }
+
+        /// <summary>
+        /// Gets the numeral represented by the given character
+        /// </summary>
+        /// <param name="ch"> The roman numberal symbol</param>
+        /// <exception cref="ArgumentException"> 
+        /// When a symbol is not recognized as a roman numeral
+        /// </exception>
+        public static  Numeral GetNumeral(char ch) {
+            char con = char.ToUpper(ch);
+            foreach (Numeral num in numerals) {
+                if (num.symbol == con) {
+                    return num;
+                }
+
+            }
+
+            throw new System.ArgumentException("Unrecognized character");
+
+        }
+
+
+        /// <summary>
+        /// Converts a decimal number into a roman numeral
+        /// </summary>
+        /// <param name="number"> The decimal number</param>
+        public static string toRoman(int number) {
+            StringBuilder builder = new StringBuilder();
+            int value = number;
+            int numIndex = 0;
+
+            while (value > 0) {
+                Numeral num = numerals[numIndex];
+                //DEBUG Console.WriteLine("  ~ Value" + value);
+                //Standard check
+                if (value >= num.value)
+                {
+                    //DEBUG Console.WriteLine("  ~ Primary" + num.symbol);
+                    builder.Append(num.symbol);
+                    value -= num.value;
+                }
+                else {
+                   //DEBUG  Console.WriteLine("  ~ FSecondary " + num.symbol);
+                    //Look for combinations
+                    Numeral? next = null;
+                    for (int search = numerals.Length - 1; search > numIndex; search--) {
+                        Numeral candidate = numerals[search];
+                       //DEBUG  Console.WriteLine("  ~ Try " + candidate.symbol);
+
+                        //Prevent Stuff like VX = 5 from happening
+                        if (num.value - candidate.value == candidate.value)
+                        {
+                           //DEBUG Console.Write("  ~ Cancel try " + candidate.symbol);
+                            continue; }
+                        if (value >= num.value - candidate.value) {
+                            next = candidate;
+                            break;
+                        }
+                    }
+
+
+                    if (next != null)
+                    { 
+                        //DEBUG Console.WriteLine("  ~ SecNext " + next.Value.symbol);
+                        builder.Append(next.Value.symbol).Append(num.symbol);
+                        value -= num.value - next.Value.value;
+                    }
+
+
+                    //Move to next numeral
+               
+                    numIndex += 1;
+                }
+
+
+
+            }
+            return builder.ToString() ;
+        }
+
+
+        /// <summary>
+        /// Converts a roman numeral sequence into a decimal number
+        /// </summary>
+        /// <param name="stuff"> The roman numberal sequence</param>
+        public static int ToDecimal(string stuff) {
+            if (stuff.Length == 0) {
+                return 0;
+            }
+
+
+            int value = 0;
+            for (int i = 0; i < stuff.Length -1; i++) {
+                Numeral norm = GetNumeral(stuff[i]);
+                Numeral next = GetNumeral(stuff[i + 1]);
+
+                if (next.value > norm.value)
+                {
+                    value -= norm.value;
+                }
+                else {
+                    value += norm.value;
+                }
+            }
+
+            value += GetNumeral(stuff[stuff.Length - 1]).value;
+
+            return value;
+        }
+    }
+
+    /// <summary>
+    /// Represents the decimal value and character symbol of a roman numeral
+    /// </summary>
+    public struct Numeral{
+        public readonly char symbol;
+        public readonly int value;
+
+
+        public Numeral(char sym, int val) {
+            this.symbol = sym;
+            this.value = val;
+        }
+    }
+}
